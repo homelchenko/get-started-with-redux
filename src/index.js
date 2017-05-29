9c1925a0c55856fbc1288a973a8b6dffe84ceefa
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2017-05-29 19:13:43 +0300

    Change main component from App to Counter subscribing to store changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { store } from './counterReducer';
+
+import Counter from './counter';
+
+const render = () => {
+    ReactDOM.render(
+        <Counter 
+            value={store.getState()}
+            onIncrement={() => 
+                store.dispatch({
+                    type: 'INCREMENT'
+                })
+            }
+            onDecrement={() =>
+                store.dispatch({
+                    type: 'DECREMENT'
+                })
+            }
+        />,
+        document.getElementById('root')
+    );
+}
+
+store.subscribe(() => render());
+render();

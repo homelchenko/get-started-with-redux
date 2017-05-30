b7546d9e1fc1d13dda966a734d24e680e36d0d9b
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2017-05-30 17:38:54 +0300

    Use a separate reducer for a todo item

--- src/todoReducer.js
+++ src/todoReducer.js
@@ -1,27 +1,40 @@
-export default (state = [], action) => {
+const todos = (state = [], action) => {
     switch(action.type) {
         case 'ADD_TODO':
             return [
                 ...state,
-                {
-                    id: action.id,
-                    text: action.text,
-                    completed: false
-                }
+                todo(undefined, action)
         ];
         case 'TOGGLE_TODO':
-            return state.map(todo => {
-                if (todo.id !== action.id){
-                    return todo;
-                }
-
-                return {
-                    ...todo,
-                    completed: !todo.completed
-                };
-            });
+            return state.map(todo => todo(todo, action));
         default:
             return state;
     }
 
-}
+};
+
+const todo = (state, action) => {
+    switch(action.type) {
+        case 'ADD_TODO':
+            return {
+                id: action.id,
+                text: action.text,
+                completed: action.completed
+            };
+        case 'TOGGLE_TODO':
+            if (state.id !== action.id){
+                return state;
+            }
+            
+            return {
+                ...state,
+                completed: !state.completed
+            };
+        default:
+            return state;
+    }    
+};
+
+export default todos;
+
+export { todo };

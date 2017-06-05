63ae5a424ee1bca238114dfac7aaba143b8504d7
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2017-06-05 17:47:17 +0300

    Pre-define to actions to be dispatched after app starts

--- src/index.js
+++ src/index.js
@@ -5,6 +5,18 @@ import store from './store';
 
 import TodoApp from './todoAppComponent';
 
+store.dispatch({
+    type: 'ADD_TODO',
+    text: 'Buy milk',
+    completed: false
+});
+
+store.dispatch({
+    type: 'ADD_TODO',
+    text: 'Do the dishes',
+    completed: true
+});
+
 const render = () => {
     ReactDOM.render(
         <TodoApp

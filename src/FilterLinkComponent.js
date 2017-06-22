781fd9df7ae6e0a61b77c472a98f2c4544164e14
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2017-06-22 23:03:35 +0300

    Refactor toProps functions

--- src/FilterLinkComponent.js
+++ src/FilterLinkComponent.js
@@ -6,23 +6,18 @@ import Link from './linkComponent';
 const stateToProps = (
     state,
     ownProps
-) => {
-    return {
-        active: ownProps.filter === state.visibilityFilter
-    };
-};
+) => ({
+    active: ownProps.filter === state.visibilityFilter
+});
 
 const dispatchToProps = (
     dispatch,
     ownProps
-) => {
-    return {
-        onClick: () =>
-            dispatch(
-                setVisibilityFilter(ownProps.filter)
-            )
-    };
-};
+) => ({
+    onClick() {
+        dispatch(setVisibilityFilter(ownProps.filter))
+    }
+});
 
 const FilterLinkContaner = connect(
     stateToProps,

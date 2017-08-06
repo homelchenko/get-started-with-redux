41e588d3f802a829278b8300a390bfb4a7fe4be1
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2017-08-06 18:50:25 +0300

    Move fetching data to a new visibletodolist component

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -1,21 +1,44 @@
+import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-
 
 import { toggleTodo } from '../actions';
 import TodoList from './TodoList';
 import { getVisibleTodos } from '../reducers';
+import { fetchTodos } from '../api/';
 
-const mapStateToProps = (state, { match: { params }}) => ({
-    todos: getVisibleTodos(
-        state,
-        params.filter || 'all'
-    )
-});
+class VisibleTodoList extends Component {
+    componentDidMount() {
+        fetchTodos(this.props.filter).then(todos =>
+            console.log(this.props.filter, todos)
+        );
+    }
 
-const VisibleTodoList = withRouter(connect(
+    componentDidUpdate(prevProps) {
+        if (this.props.filtter !== prevProps.filter) {
+            fetchTodos(this.props.filter).then(todos =>
+                console.log(this.props.filter, todos)
+            );
+        }
+    }
+
+    render() {
+        return <TodoList {...this.props} />
+    }
+}
+
+const mapStateToProps = (state, { match: { params }}) => {
+    const filter = params.filter || 'all';
+
+    return {
+        todos: getVisibleTodos(state, filter),
+        filter
+    };
+};
+
+VisibleTodoList = withRouter(connect(
     mapStateToProps,
     { onTodoClick: toggleTodo }
-)(TodoList));
+)(VisibleTodoList));
 
 export default VisibleTodoList ;
--- src/configureStore.js
+++ src/configureStore.js
@@ -2,11 +2,6 @@ import { createStore } from 'redux';
 
 import addLoggingToDispatch from './loggingDispatch';
 import reducer from './reducers';
-import { fetchTodos } from './api/';
-
-fetchTodos('all').then(todos => 
-    console.log(todos)
-);
 
 const configureStore = () => {
     const store = createStore(

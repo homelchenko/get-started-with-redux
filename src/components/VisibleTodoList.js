0c2c5dd20b3fe109bab3da0193d9900dc2493702
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2017-08-07 14:25:08 +0300

    Extract method to fetch data and log it to console

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -9,17 +9,19 @@ import { fetchTodos } from '../api/';
 
 class VisibleTodoList extends Component {
     componentDidMount() {
-        fetchTodos(this.props.filter).then(todos =>
-            console.log(this.props.filter, todos)
-        );
+        this.fetchData();
     }
 
     componentDidUpdate(prevProps) {
         if (this.props.filtter !== prevProps.filter) {
-            fetchTodos(this.props.filter).then(todos =>
-                console.log(this.props.filter, todos)
-            );
+            this.fetchData();
         }
+    }
+
+    fetchData() {
+        fetchTodos(this.props.filter).then(todos =>
+            console.log(this.props.filter, todos)
+        );
     }
 
     render() {

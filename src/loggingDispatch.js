f11fa96b496459155bf1ef7f69820f247c4c0c13
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2017-08-17 14:10:37 +0300

    Use array functions to simplify logging middleware

--- src/loggingDispatch.js
+++ src/loggingDispatch.js
@@ -2,23 +2,21 @@ const addLoggingToDispatch = (store) => {
     return logging(store)(store.dispatch);
 }
 
-export const logging = (store) => {
-    return (next) => {
-        if (!console.group) {
-            return next;
-        }
+export const logging = (store) => (next) => {
+    if (!console.group) {
+        return next;
+    }
 
-        return (action) => {
-            console.group(action.type);
-            console.log('%c prev state', 'color: gray', store.getState());
-            console.log('%c action', 'color: blue', action);
-            const returnValue = next(action);
-            console.log('%c next state', 'color: green', store.getState());
-            console.groupEnd(action.type);
+    return (action) => {
+        console.group(action.type);
+        console.log('%c prev state', 'color: gray', store.getState());
+        console.log('%c action', 'color: blue', action);
+        const returnValue = next(action);
+        console.log('%c next state', 'color: green', store.getState());
+        console.groupEnd(action.type);
 
-            return returnValue;
-        }
-    };
-}
+        return returnValue;
+    }
+};
 
 export default addLoggingToDispatch;

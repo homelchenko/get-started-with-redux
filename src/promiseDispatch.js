7a0d1c5580bed4768ab712924cfc635fc0cd61d2
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2017-08-17 13:53:09 +0300

    Rename local variable

--- src/promiseDispatch.js
+++ src/promiseDispatch.js
@@ -1,12 +1,12 @@
 const addPromiseSupportToDispatch = (store) => {
-    const rawDispatch = store.dispatch;
+    const next = store.dispatch;
 
     return (action) => {
         if (typeof action.then === 'function') {
-            return action.then(rawDispatch);
+            return action.then(next);
         }
 
-        return rawDispatch;
+        return next;
     }
 }
 

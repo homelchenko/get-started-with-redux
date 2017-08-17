7efbdcca85e698fbbe11d93020f771544859a777
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2017-08-17 13:53:49 +0300

    Rename local variable

--- src/loggingDispatch.js
+++ src/loggingDispatch.js
@@ -3,13 +3,13 @@ const addLoggingToDispatch = (store) => {
         return store.dispatch;
     }
     
-    const rawDispatch = store.dispatch;
+    const next = store.dispatch;
     
     return (action) => {
         console.group(action.type);
         console.log('%c prev state', 'color: gray', store.getState());
         console.log('%c action', 'color: blue', action);
-        const returnValue = rawDispatch(action);
+        const returnValue = next(action);
         console.log('%c next state', 'color: green', store.getState());
         console.groupEnd(action.type);
 

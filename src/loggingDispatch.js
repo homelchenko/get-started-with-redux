fa6a97f501c83482a8c87889df5d1db4fc0325f5
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2017-08-17 14:05:08 +0300

    Extract logging middleware function

--- src/loggingDispatch.js
+++ src/loggingDispatch.js
@@ -1,19 +1,23 @@
 const addLoggingToDispatch = (store) => {
-    if (!console.group) {
-        return store.dispatch;
-    }
-    
-    const next = store.dispatch;
-    
-    return (action) => {
-        console.group(action.type);
-        console.log('%c prev state', 'color: gray', store.getState());
-        console.log('%c action', 'color: blue', action);
-        const returnValue = next(action);
-        console.log('%c next state', 'color: green', store.getState());
-        console.groupEnd(action.type);
+    return logging(store.dispatch)(store);
+}
 
-        return returnValue;
+export const logging = (next) => {
+    return (store) => {
+        if (!console.group) {
+            return next;
+        }
+
+        return (action) => {
+            console.group(action.type);
+            console.log('%c prev state', 'color: gray', store.getState());
+            console.log('%c action', 'color: blue', action);
+            const returnValue = next(action);
+            console.log('%c next state', 'color: green', store.getState());
+            console.groupEnd(action.type);
+
+            return returnValue;
+        }
     };
 }
 

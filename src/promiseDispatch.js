704ca50469aecaaf19555c2c27ec71d94389ee49
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2017-08-17 14:11:43 +0300

    Use arrow functions to simplify promise middleware

--- src/promiseDispatch.js
+++ src/promiseDispatch.js
@@ -2,16 +2,12 @@ const addPromiseSupportToDispatch = (store) => {
     return promise(store)(store.dispatch);
 }
 
-export const promise = (store) => {
-    return (next) => {
-        return (action) => {
-            if (typeof action.then === 'function') {
-                return action.then(next);
-            }
-
-            return next;
-        }
+export const promise = (store) => (next) => (action) => {
+    if (typeof action.then === 'function') {
+        return action.then(next);
     }
+
+    return next;
 }
 
 export default addPromiseSupportToDispatch;

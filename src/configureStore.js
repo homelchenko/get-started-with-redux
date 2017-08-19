15624e938dac12d77a2f964b749c95dd0d2b8749
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2017-08-19 18:12:27 +0300

    Use Redux's promise middleware

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise'
 
 import logger from './loggingDispatch';
-import promise from './promiseDispatch';
 import reducer from './reducers';
 
 const configureStore = () => {
--- src/promiseDispatch.js
+++
@@ -1,7 +0,0 @@
-export default (store) => (next) => (action) => {
-    if (typeof action.then === 'function') {
-        return action.then(next);
-    }
-
-    return next;
-}

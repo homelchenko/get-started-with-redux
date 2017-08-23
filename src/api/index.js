aa4a5ee0ff9c2dd1d6e54536e37df68eefba8fbe
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2017-08-23 16:42:01 +0300

    Change shape of predefined todo array to plain

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-import fakeDatabase from '../predefineTodoList';
+import { todos } from '../predefineTodoList';
 
 const delay = (ms) =>
     new Promise(resolve => setTimeout(resolve, ms));
@@ -7,22 +7,12 @@ export const fetchTodos = (filter) =>
     delay(500).then(() => {
         switch(filter) {
             case 'all':
-                return fakeDatabase.todos;
+                return todos;
             case 'active':
-                return getTodoByState(fakeDatabase.todos, false);
+                return todos.filter(todo => !todo.completed);
             case 'completed':
-                return getTodoByState(fakeDatabase.todos, true);
+                return todos.filter(todo => todo.completed);
             default:
                 throw new Error(`Unknown filter ${filter}`);
         }
     });
-
-const getTodoByState = (todos, isCompleted) => {
-    const ids = todos.allIds.filter(id => todos.byId[id].completed === isCompleted);
-    const byId = ids.reduce((byId, id) => byId[id] = todos.byId[id], {})
-
-    return {
-        allIds: ids,
-        byId,
-    };
-}
--- src/predefineTodoList.js
+++ src/predefineTodoList.js
@@ -1,38 +1,15 @@
 import { v4 } from 'node-uuid';
 
-export const predefineTodoList = () => {
-    const buyMilkTodo = {
-        id: v4(),
-        text: 'Buy milk',
-        completed: false
-    };
+const buyMilkTodo = {
+    id: v4(),
+    text: 'Buy milk',
+    completed: false
+};
 
-    const doDishesTodo = {
-        id: v4(),
-        text: 'Do the dishes',
-        completed: true
-    };
+const doDishesTodo = {
+    id: v4(),
+    text: 'Do the dishes',
+    completed: true
+};
 
-    const todos = [buyMilkTodo, doDishesTodo];
-
-    const allIds = todos.map(todo => todo.id);
-    const byId = todos
-        .map(todo => ({
-            id: todo.id,
-            item: todo
-        }))
-        .reduce((byId, todo) => {
-            byId[todo.id] = todo.item;
-
-            return byId;
-        }, {});
-
-    return ({
-        todos: {
-            allIds,
-            byId
-        }
-    });
-}
-
-export default predefineTodoList();
+export const todos = [buyMilkTodo, doDishesTodo];

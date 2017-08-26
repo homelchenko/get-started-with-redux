fa0fd96a535e45f821717b7244ccf5c5c89fe572
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2017-08-26 19:43:23 +0300

    Extract byId reducer in a separate file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,19 +1,6 @@
 import { combineReducers } from 'redux';
 
-const byId = (state = {}, action) => {
-    switch(action.type) {
-        case 'RECEIVE_TODOS':
-            const newState = { ...state };
-
-            action.response.forEach(todo =>
-                newState[todo.id] = todo
-            );
-
-            return newState;
-        default:
-            return state;
-    }
-};
+import byId from './byId';
 
 const allIds = (state = [], action) => {
     if (action.filter !== 'all')
